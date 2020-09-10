a90a71225fd99a32d71cf6e9c4127d878fb680ad
motokikasai <kmomoky@hotmail.com>
2020-09-10 20:41:17 +0200

    Add logic for startTrivia

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,16 @@
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { merged_answers: string[] };
+
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
@@ -8,8 +21,15 @@ export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
-  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple&category=18`;
   const data = await fetch(endpoint);
   const json = await data.json();
-  console.log(json);
+
+  return json.results.map((question: QuestionState) => ({
+    ...question,
+    merged_answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
 };
