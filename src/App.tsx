3c8683d2655ee6bb4b12d87a5690f3cd166764b1
motokikasai <kmomoky@hotmail.com>
2020-09-10 15:16:36 +0200

    Set up basic logic for user interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+const App = () => {
+  const startTrivia = async () => {
+    //
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    //
+  };
+
+  const nextQuestion = () => {
+    //
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>hello TS Quiz!</h1>
-      </header>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
